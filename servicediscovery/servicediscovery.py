fd377b433a47a99ecc140e92f77451a7d9c8ae44
Hoa Binh <h.dinh@live.at>
2020-11-15 21:56:15 +0100

    adding stuff

--- servicediscovery/servicediscovery.py
+++ servicediscovery/servicediscovery.py
@@ -15,7 +15,10 @@ exoInstancePoolId = environ.get("EXOSCALE_INSTANCEPOOL_ID")
 exoTargetPort = environ.get("TARGET_PORT")
 sleepRate = 10
 
-# Lambda Signal Handler
+""" 
+ Lambda Signal Handler.
+ There are only two signals Snake... Do you understand?
+"""
 signalHandler = lambda sNum, sFrame : exit(0)
 signal.signal(signal.SIGTERM, signalHandler)
 signal.signal(signal.SIGINT, signalHandler)
@@ -24,6 +27,7 @@ signal.signal(signal.SIGINT, signalHandler)
 exo = exoscale.Exoscale(api_key=exoApiKey, api_secret=exoApiSecret)
 exoZoneId = exo.compute.get_zone(exoZone) # Getting the Zone ID for getting the list of instances later
 
+# You don't have much time in Shadow Scale! So gather as much information asap & get the hell out of here...
 while (True):
     # Init empty list for adding instance ip address utilizedb by prometheus
     ipData = [{
@@ -45,3 +49,5 @@ while (True):
         json.dump(ipData, file)
 
     time.sleep(sleepRate)
+
+# Snake? Snake?! Sneeeeek!
